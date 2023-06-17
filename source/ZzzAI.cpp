c06d42a18172d76489285b4571418cb89ab9df35
ProjectRebirthMu <117379071+ProjectRebirthMu@users.noreply.github.com>
2023-06-17 17:07:37 -0300

    Minor upgrades.

--- source/ZzzAI.cpp
+++ source/ZzzAI.cpp
@@ -1,6 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////
-// Opções de IA Aprimoradas
-// Melhorias foram feitas para otimizar o processamento.
+// AI System
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
@@ -153,91 +151,83 @@ int CalcAngle(float PositionX, float PositionY, float TargetX, float TargetY)
 	return TargetTheta;
 }
 
-float MoveHumming(vec3_t Position, vec3_t Angle, vec3_t TargetPosition, float Turn)
+float MoveHumming(vec3_t Position,vec3_t Angle,vec3_t TargetPosition,float Turn)
 {
 	float TargetAngle;
-	TargetAngle = CreateAngle(Position[0], Position[1], TargetPosition[0], TargetPosition[1]);
-	Angle[2] = TurnAngle2(Angle[2], TargetAngle, Turn);
-
+	TargetAngle = CreateAngle(Position[0],Position[1],TargetPosition[0],TargetPosition[1]);
+	Angle[2] = TurnAngle2(Angle[2],TargetAngle,Turn);
 	vec3_t Range;
-	VectorSubtract(Position, TargetPosition, Range);
-	float Distance = VectorLength(Range);
-
-	TargetAngle = 360.f - CreateAngle(Position[2], Distance, TargetPosition[2], 0.f);
-	Angle[0] = TurnAngle2(Angle[0], TargetAngle, Turn);
-
-	return Distance;
+	VectorSubtract(Position,TargetPosition,Range);
+	float Distance = sqrtf(Range[0]*Range[0]+Range[1]*Range[1]);
+	TargetAngle = 360.f-CreateAngle(Position[2],Distance,TargetPosition[2],0.f);
+	Angle[0] = TurnAngle2(Angle[0],TargetAngle,Turn);
+    return VectorLength(Range);
 }
 
-void MovePosition(vec3_t Position, vec3_t Angle, vec3_t Speed)
+void MovePosition(vec3_t Position,vec3_t Angle,vec3_t Speed)
 {
 	float Matrix[3][4];
-	AngleMatrix(Angle, Matrix);
-
+	AngleMatrix(Angle,Matrix);
 	vec3_t Velocity;
-	VectorRotate(Speed, Matrix, Velocity);
-
-	VectorAdd(Position, Velocity, Position);
+	VectorRotate(Speed,Matrix,Velocity);
+	VectorAdd(Position,Velocity,Position);
 }
 
-BYTE CalcTargetPos(float x, float y, int Tx, int Ty)
+BYTE CalcTargetPos ( float x, float y, int Tx, int Ty )
 {
-	BYTE position;
-	int PositionX = static_cast<int>(x / TERRAIN_SCALE);
-	int PositionY = static_cast<int>(y / TERRAIN_SCALE);
-	int TargetX = static_cast<int>(Tx / TERRAIN_SCALE);
-	int TargetY = static_cast<int>(Ty / TERRAIN_SCALE);
+    BYTE position;
+	int PositionX = (int)(x/TERRAIN_SCALE);
+	int PositionY = (int)(y/TERRAIN_SCALE);
+	int TargetX = (int)(Tx/TERRAIN_SCALE);
+	int TargetY = (int)(Ty/TERRAIN_SCALE);
 
-	BYTE dx = static_cast<BYTE>(8 + TargetX - PositionX);
-	BYTE dy = static_cast<BYTE>(8 + TargetY - PositionY);
+    BYTE dx = 8 + TargetX - PositionX;
+    BYTE dy = 8 + TargetY - PositionY;
 
-	position = static_cast<BYTE>((dx | (dy << 4)));
+    position = ((BYTE)( (BYTE)dx | (BYTE)(dy<<4) ) );
 
-	return position;
+    return position;
 }
 
-void Alpha(OBJECT* o)
+void Alpha(OBJECT *o)
 {
-	if (o->AlphaEnable)
+	if(o->AlphaEnable)
 	{
-		if (o->AlphaTarget > o->Alpha)
+		if(o->AlphaTarget > o->Alpha)
 		{
 			o->Alpha += 0.05f;
-			if (o->Alpha > 1.f) o->Alpha = 1.f;
+			if(o->Alpha > 1.f) o->Alpha = 1.f;
 		}
-		else if (o->AlphaTarget < o->Alpha)
+		else if(o->AlphaTarget < o->Alpha)
 		{
 			o->Alpha -= 0.05f;
-			if (o->Alpha < 0.f) o->Alpha = 0.f;
+			if(o->Alpha < 0.f) o->Alpha = 0.f;
 		}
 	}
 	else
-		o->Alpha += (o->AlphaTarget - o->Alpha) * 0.1f;
-
-	o->BlendMeshLight = min(o->Alpha, o->BlendMeshLight);
+		o->Alpha += (o->AlphaTarget-o->Alpha)*0.1f;
+	if(o->BlendMeshLight > o->Alpha)
+		o->BlendMeshLight = o->Alpha;
 }
 
-void MoveBoid(OBJECT* o, int i, OBJECT* Boids, int MAX)
+void MoveBoid(OBJECT *o,int i,OBJECT *Boids,int MAX)
 {
 	int NumBirds = 0;
 	float TargetX = 0.f;
 	float TargetY = 0.f;
-
-	for (int j = 0; j < MAX; j++)
+	for(int j=0;j<MAX;j++)
 	{
-		OBJECT* t = &Boids[j];
-		if (t->Live && j != i)
+		OBJECT *t = &Boids[j];
+		if(t->Live && j!=i)
 		{
 			vec3_t Range;
-			VectorSubtract(o->Position, t->Position, Range);
-			float Distance = VectorLength(Range);
-
-			if (Distance < 400.f)
+			VectorSubtract(o->Position,t->Position,Range);
+			float Distance = sqrtf(Range[0]*Range[0]+Range[1]*Range[1]);
+			if(Distance < 400.f)
 			{
 				float xdist = t->Direction[0] - t->Position[0];
 				float ydist = t->Direction[1] - t->Position[1];
-
-				if (Distance < 80.f)
+				if(Distance < 80.f)
 				{
 					xdist -= t->Direction[0] - o->Position[0];
 					ydist -= t->Direction[1] - o->Position[1];
@@ -247,37 +237,34 @@ void MoveBoid(OBJECT* o, int i, OBJECT* Boids, int MAX)
 					xdist += t->Direction[0] - o->Position[0];
 					ydist += t->Direction[1] - o->Position[1];
 				}
-
-				float pdist = sqrtf(xdist * xdist + ydist * ydist);
+				float pdist = sqrtf(xdist*xdist + ydist*ydist);
 				TargetX += xdist / pdist;
 				TargetY += ydist / pdist;
 				NumBirds++;
 			}
 		}
 	}
-
-	if (NumBirds > 0)
+	if(NumBirds > 0)
 	{
-		TargetX = o->Position[0] + TargetX / NumBirds;
-		TargetY = o->Position[1] + TargetY / NumBirds;
-
-		o->Angle[2] = (float)TurnAngle((int)o->Angle[2], CalcAngle(o->Position[0], o->Position[1], TargetX, TargetY), (int)o->Gravity);
+		TargetX = o->Position[0] + TargetX/NumBirds;
+		TargetY = o->Position[1] + TargetY/NumBirds;
+		
+		o->Angle[2] = (float)TurnAngle((int)o->Angle[2],CalcAngle(o->Position[0],o->Position[1],TargetX,TargetY),(int)o->Gravity);
 	}
 }
 
-void PushObject(vec3_t PushPosition, vec3_t Position, float Power, vec3_t Angle)
+void PushObject(vec3_t PushPosition,vec3_t Position,float Power,vec3_t Angle)
 {
-	Vector(0.f, 0.f, 0.f, Angle);
-	Angle[2] = CreateAngle(PushPosition[0], PushPosition[1], Position[0], Position[1]) + 180.f;
-	if (Angle[2] >= 360.f)
-		Angle[2] -= 360.f;
+	Vector(0.f,0.f,0.f,Angle);
+	Angle[2] = CreateAngle(PushPosition[0],PushPosition[1],Position[0],Position[1])+180.f;
+	if(Angle[2] >= 360.f) Angle[2] -= 360.f;
 
 	float Matrix[3][4];
-	AngleMatrix(Angle, Matrix);
-	vec3_t p1, p2;
-	Vector(0.f, -Power, 0.f, p1);
-	VectorRotate(p1, Matrix, p2);
-	Position[2] = RequestTerrainHeight(Position[0], Position[1]);
+	AngleMatrix(Angle,Matrix);
+	vec3_t p1,p2;
+	Vector(0.f,-Power,0.f,p1);
+	VectorRotate(p1,Matrix,p2);
+	Position[2] = RequestTerrainHeight(Position[0],Position[1]);
 }
 
 void SetAction_Fenrir_Skill(CHARACTER* c, OBJECT* o)
@@ -337,7 +324,7 @@ void SetAction_Fenrir_Damage(CHARACTER* c, OBJECT* o)
 		SetAction(o, PLAYER_FENRIR_DAMAGE_ONE_LEFT);
 	else if(c->Weapon[0].Type != -1 && c->Weapon[1].Type != -1 && c->Weapon[0].Type ==MODEL_BOW+15)
 		SetAction(o, PLAYER_FENRIR_DAMAGE_ONE_LEFT);
-	else
+	else	// ¸Ç¼Õ
 		SetAction(o, PLAYER_FENRIR_DAMAGE);
 #ifdef PBG_ADD_NEWCHAR_MONK_ANI
 	}
@@ -437,7 +424,6 @@ void SetAction_Fenrir_Walk(CHARACTER* c, OBJECT* o)
 	}
 #endif //PBG_ADD_NEWCHAR_MONK_ANI
 }
-
 #ifdef PBG_ADD_NEWCHAR_MONK_ANI
 bool IsAliceRideAction_UniDino(unsigned short byAction)
 #else //PBG_ADD_NEWCHAR_MONK_ANI
@@ -477,20 +463,19 @@ bool IsAliceRideAction_Fenrir(BYTE byAction)
 }
 
 
-void SetAction(OBJECT* o, int Action, bool bBlending)
+void SetAction(OBJECT *o,int Action, bool bBlending)
 {
-	BMD* b = &Models[o->Type];
-	if (Action >= b->NumActions)
-		return;
-	if (o->CurrentAction != Action)
+	BMD *b = &Models[o->Type];
+	if(Action >= b->NumActions) return;
+	if(o->CurrentAction != Action)
 	{
 		o->PriorAction = o->CurrentAction;
 		o->PriorAnimationFrame = o->AnimationFrame;
 		o->CurrentAction = Action;
 		o->AnimationFrame = 0;
-		if (!bBlending)
+		if(bBlending == false)
 		{
-			o->PriorAnimationFrame = 0;
+			o->PriorAnimationFrame = 0;	
 		}
 	}
 }
@@ -505,23 +490,17 @@ bool TestDistance(CHARACTER *c,vec3_t TargetPosition,float Range)
 	return false;
 }
 
-void LookAtTarget(OBJECT* o, CHARACTER* tc)
+void LookAtTarget(OBJECT *o,CHARACTER *tc)
 {
-	if (tc == NULL) return;
-	OBJECT* to = &tc->Object;
-	float Angle = CreateAngle(o->Position[0], o->Position[1], to->Position[0], to->Position[1]);
-	float deltaAngle = FarAngle(o->Angle[2], Angle);
-
-	if (deltaAngle < 90.f)
+	if(tc == NULL) return;
+	OBJECT *to = &tc->Object;
+	float Angle = CreateAngle(o->Position[0],o->Position[1],to->Position[0],to->Position[1]);
+	if(FarAngle(o->Angle[2],Angle) < 90.f)
 	{
-		o->HeadTargetAngle[0] = o->Angle[2] - Angle;
-		o->HeadTargetAngle[1] = (to->Position[2] - (o->Position[2] + 50.f)) * 0.2f;
-
-		if (o->HeadTargetAngle[0] < 0)
-			o->HeadTargetAngle[0] += 360.f;
-
-		if (o->HeadTargetAngle[1] < 0)
-			o->HeadTargetAngle[1] += 360.f;
+		o->HeadTargetAngle[0] = (o->Angle[2]-Angle);
+		o->HeadTargetAngle[1] = (to->Position[2]-(o->Position[2]+50.f))*0.2f;
+		for(int i=0;i<2;i++)
+			if(o->HeadTargetAngle[i] < 0) o->HeadTargetAngle[i] += 360.f;
 	}
 	else
 	{
@@ -530,29 +509,25 @@ void LookAtTarget(OBJECT* o, CHARACTER* tc)
 	}
 }
 
-void MoveHead(CHARACTER* c)
+void MoveHead(CHARACTER *c)
 {
-	OBJECT* o = &c->Object;
-
-	if (o->CurrentAction != MONSTER01_DIE)
+	OBJECT *o = &c->Object;
+	if(o->CurrentAction != MONSTER01_DIE)
 	{
-		if (o->CurrentAction == MONSTER01_STOP1)
+		if(o->CurrentAction==MONSTER01_STOP1)
 		{
-			if (rand() % 32 == 0)
+			if(rand()%32==0)
 			{
-				o->HeadTargetAngle[0] = static_cast<float>(rand() % 128 - 64);
-				o->HeadTargetAngle[1] = static_cast<float>(rand() % 48 - 16);
-
-				if (o->HeadTargetAngle[0] < 0)
-					o->HeadTargetAngle[0] += 360.f;
-
-				if (o->HeadTargetAngle[1] < 0)
-					o->HeadTargetAngle[1] += 360.f;
+				o->HeadTargetAngle[0] = (float)(rand()%128-64);
+				o->HeadTargetAngle[1] = (float)(rand()%48-16);
+				for(int i=0;i<2;i++)
+					if(o->HeadTargetAngle[i] < 0) o->HeadTargetAngle[i] += 360.f;
 			}
 		}
-		else if (o->CurrentAction == MONSTER01_WALK && c->TargetCharacter != -1)
+		else
+		if(o->CurrentAction==MONSTER01_WALK && c->TargetCharacter != -1)
 		{
-			LookAtTarget(o, &CharactersClient[c->TargetCharacter]);
+			LookAtTarget(o,&CharactersClient[c->TargetCharacter]);
 		}
 		else
 		{
@@ -562,22 +537,19 @@ void MoveHead(CHARACTER* c)
 	}
 }
 
-void Damage(vec3_t soPosition, CHARACTER* tc, float AttackRange, int AttackPoint, bool Hit)
+void Damage(vec3_t soPosition,CHARACTER *tc,float AttackRange,int AttackPoint,bool Hit)
 {
-	if (tc == NULL)
-		return;
+	return;
+	if(tc == NULL) return;
 
-	OBJECT* to = &tc->Object;
+	OBJECT *to = &tc->Object;
 
-	// Attack
-	if (AttackPoint <= 0)
-		Hit = false;
-	else if (AttackPoint >= 20)
-		Hit = true;
-
-	if (Hit)
+	//attack
+	if     (AttackPoint <= 0 ) Hit = false;
+	else if(AttackPoint >= 20) Hit = true;
+	if(Hit)
 	{
-		switch (to->Type)
+		switch(to->Type)
 		{
 		case MODEL_MONSTER01:
 			break;
@@ -587,113 +559,100 @@ void Damage(vec3_t soPosition, CHARACTER* tc, float AttackRange, int AttackPoint
 	}
 	else
 	{
-		switch (to->Type)
+		switch(to->Type)
 		{
 		case MODEL_MONSTER01:
 			break;
 		case MODEL_PLAYER:
-			break;
+ 			break;
 		}
 	}
-
 	vec3_t Position;
-
-	if (AttackPoint > 0)
+	if(AttackPoint > 0)
 	{
 		vec3_t Range;
-		VectorSubtract(soPosition, to->Position, Range);
-		VectorMA(to->Position, 0.3f, Range, Position);
+		VectorSubtract(soPosition,to->Position,Range);
+		VectorMA(to->Position,0.3f,Range,Position);
 		Position[2] += 80.f;
 		vec3_t Light;
-		Vector(1.f, 1.f, 1.f, Light);
-
-		for (int i = 0; i < 40; i++)
+		Vector(1.f,1.f,1.f,Light);
+		for(int i=0;i<40;i++) 
 		{
-			CreateParticle(BITMAP_SPARK, Position, to->Angle, Light);
+			CreateParticle(BITMAP_SPARK,Position,to->Angle,Light);
 			vec3_t Angle;
-			Vector(-(float)(rand() % 60 + 30), 0.f, (float)(rand() % 360), Angle);
-			CreateJoint(BITMAP_JOINT_SPARK, Position, Position, Angle);
+			Vector(-(float)(rand()%60+30),0.f,(float)(rand()%360),Angle);
+			CreateJoint(BITMAP_JOINT_SPARK,Position,Position,Angle);
 		}
-
-		CreateParticle(BITMAP_SPARK + 1, Position, to->Angle, Light);
+		CreateParticle(BITMAP_SPARK+1,Position,to->Angle,Light);
 	}
 
-	// Point
-	VectorCopy(to->Position, Position);
-	Position[2] += 130.f + to->CollisionRange * 0.5f;
+	//point
+	VectorCopy(to->Position,Position);
+	Position[2] += 130.f+to->CollisionRange*0.5f;
 	vec3_t Color;
-	Vector(1.f, 0.1f, 0.1f, Color);
-	CreatePoint(Position, AttackPoint, Color);
+	Vector(1.f,0.1f,0.1f,Color);
+	CreatePoint(Position,AttackPoint,Color);
 }
 
-bool MovePath(CHARACTER* c, bool Turn)
+bool MovePath(CHARACTER *c,bool Turn)
 {
 	bool Success = false;
-	PATH_t* p = &c->Path;
-
-	if (p->CurrentPath < p->PathNum)
+	PATH_t *p = &c->Path;
+	if(p->CurrentPath < p->PathNum)
 	{
-		if (p->CurrentPathFloat == 0)
+		if(p->CurrentPathFloat == 0)
 		{
-			int Path = p->CurrentPath + 1;
-			if (Path > p->PathNum - 1)
-				Path = p->PathNum - 1;
-
+			int Path = p->CurrentPath+1;
+			if(Path > p->PathNum-1)
+				Path = p->PathNum-1;
 			c->PositionX = p->PathX[Path];
 			c->PositionY = p->PathY[Path];
 		}
-
-		float x[4], y[4];
-		for (int i = 0; i < 4; i++)
+		float x[4],y[4];
+		for(int i=0;i<4;i++)
 		{
-			int Path = p->CurrentPath + i - 1;
-			if (Path < 0)
+			int Path = p->CurrentPath+i-1;
+			if(Path < 0)
 				Path = 0;
-			else if (Path > p->PathNum - 1)
-				Path = p->PathNum - 1;
-
-			x[i] = ((float)p->PathX[Path] + 0.5f) * TERRAIN_SCALE;
-			y[i] = ((float)p->PathY[Path] + 0.5f) * TERRAIN_SCALE;
+			else if(Path > p->PathNum-1)
+				Path = p->PathNum-1;
+		    x[i] = ((float)p->PathX[Path]+0.5f)*TERRAIN_SCALE;
+		    y[i] = ((float)p->PathY[Path]+0.5f)*TERRAIN_SCALE;
 		}
-
-		float cx, cy;
-		switch (p->CurrentPathFloat)
+		float cx,cy;
+		switch(p->CurrentPathFloat)
 		{
 		case 0:
-			cx = x[0] * -0.0703125f + x[1] * 0.8671875f + x[2] * 0.2265625f + x[3] * -0.0234375f;
-			cy = y[0] * -0.0703125f + y[1] * 0.8671875f + y[2] * 0.2265625f + y[3] * -0.0234375f;
+			cx = x[0]*-0.0703125f + x[1]*0.8671875f + x[2]*0.2265625f + x[3]*-0.0234375f;
+			cy = y[0]*-0.0703125f + y[1]*0.8671875f + y[2]*0.2265625f + y[3]*-0.0234375f;
 			break;
 		case 1:
-			cx = x[0] * -0.0625f + x[1] * 0.5625f + x[2] * 0.5625f + x[3] * -0.0625f;
-			cy = y[0] * -0.0625f + y[1] * 0.5625f + y[2] * 0.5625f + y[3] * -0.0625f;
+			cx = x[0]*-0.0625f + x[1]*0.5625f + x[2]*0.5625f + x[3]*-0.0625f;
+			cy = y[0]*-0.0625f + y[1]*0.5625f + y[2]*0.5625f + y[3]*-0.0625f;
 			break;
 		case 2:
-			cx = x[0] * -0.0234375f + x[1] * 0.2265625f + x[2] * 0.8671875f + x[3] * -0.0703125f;
-			cy = y[0] * -0.0234375f + y[1] * 0.2265625f + y[2] * 0.8671875f + y[3] * -0.0703125f;
+			cx = x[0]*-0.0234375f + x[1]*0.2265625f + x[2]*0.8671875f + x[3]*-0.0703125f;
+			cy = y[0]*-0.0234375f + y[1]*0.2265625f + y[2]*0.8671875f + y[3]*-0.0703125f;
 			break;
 		case 3:
 			cx = x[2];
 			cy = y[2];
 			break;
 		}
-
-		OBJECT* o = &c->Object;
-		float dx = o->Position[0] - cx;
-		float dy = o->Position[1] - cy;
-		float Distance = sqrtf(dx * dx + dy * dy);
-
-		if (Distance <= 20.f)
+     	OBJECT *o = &c->Object;
+		float dx = o->Position[0]-cx;
+		float dy = o->Position[1]-cy;
+		float Distance = sqrtf(dx*dx+dy*dy);
+		if(Distance <= 20.f)
 		{
 			p->CurrentPathFloat++;
-
-			if (p->CurrentPathFloat > 3)
+			if(p->CurrentPathFloat > 3)
 			{
 				p->CurrentPathFloat = 0;
 				p->CurrentPath++;
-
-				if (p->CurrentPath >= p->PathNum - 1)
+				if(p->CurrentPath >= p->PathNum-1)
 				{
-					p->CurrentPath = p->PathNum - 1;
+					p->CurrentPath = p->PathNum-1;
 					c->PositionX = p->PathX[p->CurrentPath];
 					c->PositionY = p->PathY[p->CurrentPath];
 					o->Position[0] = cx;
@@ -702,19 +661,17 @@ bool MovePath(CHARACTER* c, bool Turn)
 				}
 			}
 		}
-
-		if (!Success && Turn)
+		if(!Success && Turn)
 		{
-			float Angle = CreateAngle(o->Position[0], o->Position[1], cx, cy);
-			float TargetAngle = FarAngle(o->Angle[2], Angle);
-
-			if (TargetAngle >= 45.f)
+         	float Angle = CreateAngle(o->Position[0],o->Position[1],cx,cy);
+			float TargetAngle = FarAngle(o->Angle[2],Angle);
+			if(TargetAngle >= 45.f)
 			{
-				o->Angle[2] = Angle;
+            	o->Angle[2] = Angle;
 			}
 			else
 			{
-				o->Angle[2] = TurnAngle2(o->Angle[2], Angle, TargetAngle * 0.5f);
+      			o->Angle[2] = TurnAngle2(o->Angle[2],Angle,TargetAngle*0.5f);
 			}
 		}
 	}
@@ -723,6 +680,7 @@ bool MovePath(CHARACTER* c, bool Turn)
 }
 
 #ifdef SAVE_PATH_TIME
+
 void WriteDebugInfoStr( char *lpszFileName, char *lpszToWrite)
 {
 	HANDLE hFile = CreateFile( lpszFileName, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS,
@@ -743,50 +701,53 @@ void DebugUtil_Write( char *lpszFileName, ...)
 	WriteDebugInfoStr( lpszFileName, lpszBuffer);
 	va_end( va);
 }
-#endif // SAVE_PATH_TIME
 
-PATH* path = new PATH();
+#endif
+
+
+PATH* path  = new PATH;
 
 void InitPath()
 {
-	path->SetMapDimensions(256, 256, TerrainWall);
+    path->SetMapDimensions(256,256,TerrainWall);
 }
 
-bool PathFinding2(int sx, int sy, int tx, int ty, PATH_t* a, float fDistance, int iDefaultWall)
+bool PathFinding2(int sx,int sy,int tx,int ty,PATH_t *a, float fDistance, int iDefaultWall)
 {
 	bool Value = false;
 
-	if (M34CryWolf1st::Get_State_Only_Elf() && M34CryWolf1st::IsCyrWolf1st())
+	if(M34CryWolf1st::Get_State_Only_Elf() == true && M34CryWolf1st::IsCyrWolf1st() == true)
 	{
-		if (CharactersClient[TargetNpc].Object.Type >= MODEL_CRYWOLF_ALTAR1 && CharactersClient[TargetNpc].Object.Type <= MODEL_CRYWOLF_ALTAR5)
+		if((CharactersClient[TargetNpc].Object.Type >= MODEL_CRYWOLF_ALTAR1 && CharactersClient[TargetNpc].Object.Type <= MODEL_CRYWOLF_ALTAR5))
 		{
 			Value = true;
 		}
 	}
 
+
 	int Wall = iDefaultWall;
 
 	bool Success = path->FindPath(sx, sy, tx, ty, true, Wall, Value, fDistance);
-	if (!Success)
+	if(!Success)
 	{
-		if ((TerrainWall[TERRAIN_INDEX_REPEAT(sx, sy)] & TW_SAFEZONE) == TW_SAFEZONE || (TerrainWall[TERRAIN_INDEX_REPEAT(tx, ty)] & TW_SAFEZONE) == TW_SAFEZONE && (TerrainWall[TERRAIN_INDEX_REPEAT(tx, ty)] & TW_CHARACTER) != TW_CHARACTER)
+		if( ((TerrainWall[TERRAIN_INDEX_REPEAT(sx,sy)]&TW_SAFEZONE) == TW_SAFEZONE || (TerrainWall[TERRAIN_INDEX_REPEAT(tx,ty)]&TW_SAFEZONE) == TW_SAFEZONE) && (TerrainWall[TERRAIN_INDEX_REPEAT(tx,ty)]&TW_CHARACTER) != TW_CHARACTER )
 		{
 			Wall = TW_NOMOVE;
 		}
 
-		Success = path->FindPath(sx, sy, tx, ty, false, Wall, Value, fDistance);
+    	Success = path->FindPath(sx, sy, tx, ty, false, Wall, Value, fDistance);
 	}
 
-	if (Success)
+	if(Success)
 	{
 		int PathNum = path->GetPath();
-		if (PathNum > 1)
+		if(PathNum > 1)
 		{
 			a->PathNum = PathNum;
-			unsigned char* x = path->GetPathX();
-			unsigned char* y = path->GetPathY();
+			unsigned char *x = path->GetPathX();
+			unsigned char *y = path->GetPathY();
 
-			for (int i = 0; i < a->PathNum; i++)
+			for(int i=0;i<a->PathNum;i++)
 			{
 				a->PathX[i] = x[i];
 				a->PathY[i] = y[i];
@@ -848,3 +809,4 @@ void CalcFPS()
 		gSkillManager.CalcSkillDelay(difTime);
 	}
 }
+
